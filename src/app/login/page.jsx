7dee3801b007c156eea46452589a84a52d78e417
anayra-c6b9 <anayra13@proton.me>
2023-10-01 16:27:15 +0530

    demo added login page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <main>
+      <h1 className="text-blue-600 mob:text-lg lg:text-5xl">
+        This is login page
+      </h1>
+    </main>
+  );
+}
