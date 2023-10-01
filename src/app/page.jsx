b2728b3b886dec17db8fbc858f3987af48cb71b2
anayra-c6b9 <anayra13@proton.me>
2023-10-01 14:25:18 +0530

    Configured tailwind.config.js
    added media screen width for mobile device in ./tailwind.config.js

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-blue-600 mob:text-lg lg:text-5xl">Hello world</h1>
+    </main>
+  );
+}
